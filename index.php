16e6ea5e918db3868311795916ad48288d886523
valeria <leradopira@mail.ru>
2019-11-21 17:04:39 +0300

    Added container

--- index.php
+++ index.php
@@ -31,6 +31,40 @@
                 <p>This is Phase Shift, a responsive HTML5 site template freebie by TEMPLATED.</p>
                 <a href="item.html" class="button medium">Read More</a>
         </div>
+
+
+        <div id="main">
+            <div class="container">
+                <div class="row">
+                    <div class="6u">
+                        <section>
+                            <ul class="style">
+                                <li class="fa fa-wrench">
+                                    <h3>Integer ultrices</h3>
+                                    <span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span> </li>
+                                <li class="fa fa-leaf">
+                                    <h3>Aliquam luctus</h3>
+                                    <span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span> </li>
+                            </ul>
+                        </section>
+                    </div>
+
+                    <div class="6u">
+                        <section>
+                            <ul class="style">
+                                <li class="fa fa-cogs">
+                                    <h3>Integer ultrices</h3>
+                                    <span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span> </li>
+                                <li class="fa fa-road">
+                                    <h3>Aliquam luctus</h3>
+                                    <span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span> </li>
+                            </ul>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
     <?php
